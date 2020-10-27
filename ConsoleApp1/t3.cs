1197e5e38b92c787eb377e6feed10d11f0940d8e
Aleksandr Ivanov <69570553+Aleksandr2803@users.noreply.github.com>
2020-10-27 11:06:11 +0200

    Create t3.cs

---
+++ ConsoleApp1/t3.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleApp277
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(8 + 5);
+            Console.WriteLine(8 - 5);
+            Console.WriteLine(8 * 5);
+            Console.WriteLine(8 / 5);
+        }
+    }
+}
