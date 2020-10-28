6d4cc758bc242e36c2d819e5599730dcff5b76b6
Aleksandr Ivanov <69570553+Aleksandr2803@users.noreply.github.com>
2020-10-28 09:26:00 +0200

    Create t5.cs

---
+++ ConsoleApp1/t5.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Anna nimesi:");
+
+         
+            string yourName = Console.ReadLine();
+
+            
+            Console.WriteLine("Hei: " + yourName);
+        }
+    }
+}
