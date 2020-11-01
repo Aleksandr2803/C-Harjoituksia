933c2f52e020e3d1645cbf011a41bfed3a730dab
Aleksandr Ivanov <69570553+Aleksandr2803@users.noreply.github.com>
2020-11-01 20:10:48 +0200

    Create t12.cs

---
+++ ConsoleApp1/t12.cs
@@ -0,0 +1,12 @@
+
+using System;
+ class t12
+{
+     static void Main()
+    {
+        int age;
+        Console.Write("Syötä ikäsi ");
+        age = Convert.ToInt32(Console.ReadLine());
+        Console.Write("näytät ikäistäsi nuoremmalta ", age);
+    }
+}
